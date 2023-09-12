c87e777c203d672d8de75ee77bb5c9e225dc4c28
ktk8916 <ktk8916@naver.com>
2023-09-12 16:53:20 +0900

    init

---
+++ src/test/java/com/playtable/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playtable.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
